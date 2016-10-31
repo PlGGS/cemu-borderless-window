f97648893c87cb1caa1dbc899de75bed2bc36677
PlGGS <bbor0422@gmail.com>
2016-10-30 23:22:26 -0500

    Add project files.

---
+++ CBW/frmMain.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace CBW
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCBW = new System.Windows.Forms.Label();
+            this.chkCBW = new System.Windows.Forms.CheckBox();
+            this.chkShowMenuStrip = new System.Windows.Forms.CheckBox();
+            this.lblShowMenuStrip = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCBW
+            // 
+            this.lblCBW.AutoSize = true;
+            this.lblCBW.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCBW.Location = new System.Drawing.Point(7, 9);
+            this.lblCBW.Name = "lblCBW";
+            this.lblCBW.Size = new System.Drawing.Size(128, 16);
+            this.lblCBW.TabIndex = 0;
+            this.lblCBW.Text = "Borderless Window:";
+            // 
+            // chkCBW
+            // 
+            this.chkCBW.AutoSize = true;
+            this.chkCBW.Location = new System.Drawing.Point(179, 11);
+            this.chkCBW.Name = "chkCBW";
+            this.chkCBW.Size = new System.Drawing.Size(15, 14);
+            this.chkCBW.TabIndex = 1;
+            this.chkCBW.UseVisualStyleBackColor = true;
+            this.chkCBW.CheckedChanged += new System.EventHandler(this.chkCBW_CheckedChanged);
+            // 
+            // chkShowMenuStrip
+            // 
+            this.chkShowMenuStrip.AutoSize = true;
+            this.chkShowMenuStrip.Location = new System.Drawing.Point(179, 35);
+            this.chkShowMenuStrip.Name = "chkShowMenuStrip";
+            this.chkShowMenuStrip.Size = new System.Drawing.Size(15, 14);
+            this.chkShowMenuStrip.TabIndex = 3;
+            this.chkShowMenuStrip.UseVisualStyleBackColor = true;
+            this.chkShowMenuStrip.CheckedChanged += new System.EventHandler(this.chkShowMenuStrip_CheckedChanged);
+            // 
+            // lblShowMenuStrip
+            // 
+            this.lblShowMenuStrip.AutoSize = true;
+            this.lblShowMenuStrip.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShowMenuStrip.Location = new System.Drawing.Point(7, 33);
+            this.lblShowMenuStrip.Name = "lblShowMenuStrip";
+            this.lblShowMenuStrip.Size = new System.Drawing.Size(110, 16);
+            this.lblShowMenuStrip.TabIndex = 2;
+            this.lblShowMenuStrip.Text = "Show Menu Strip:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(201, 57);
+            this.Controls.Add(this.chkShowMenuStrip);
+            this.Controls.Add(this.lblShowMenuStrip);
+            this.Controls.Add(this.chkCBW);
+            this.Controls.Add(this.lblCBW);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMain";
+            this.Text = "Cemu Borderless Window Mode";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCBW;
+        private System.Windows.Forms.CheckBox chkCBW;
+        private System.Windows.Forms.CheckBox chkShowMenuStrip;
+        private System.Windows.Forms.Label lblShowMenuStrip;
+    }
+}
+
---
+++ CBW/frmMain.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CBW
+{
+    public partial class frmMain : Form
+    {
+        //Import window changing function
+        [DllImport("USER32.DLL")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        //Import find window finding function
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        //Import force window draw function
+        [DllImport("user32.dll")]
+        static extern bool DrawMenuBar(IntPtr hWnd);
+
+        //Import window placement function
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+        private const int gwlStyle = -16;
+        private const int wsBorder = 0x00800000;
+        private const int wsCaption = 0x00C00000;
+        private const int wsSysMenu = 0x00080000;
+        private const int wsMinimizeBox = 0x00020000;
+        private const int wsMenuStrip = 19;
+        Screen[] screens = Screen.AllScreens;
+        private IntPtr window;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public static bool checkIfProcessIsRunning(string nameSubstring)
+        {
+            return Process.GetProcesses().Any(p => p.ProcessName.Contains(nameSubstring));
+        }
+
+        public void setWindow()
+        {
+            if (chkCBW.CheckState == CheckState.Checked)
+            {
+                if (chkShowMenuStrip.CheckState == CheckState.Checked)
+                {
+                    try
+                    {
+                        SetWindowLong(window, gwlStyle, wsSysMenu);
+                        SetWindowPos(window, 0, screens[0].Bounds.X, screens[0].Bounds.Y, screens[0].Bounds.Width, screens[0].Bounds.Height + wsMenuStrip, 0x0040);
+                        DrawMenuBar(window);
+                        Taskbar.Hide();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Failed to properly place Cemu into borderless window mode.", "Cemu Borderless Window");
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        SetWindowLong(window, gwlStyle, wsSysMenu);
+                        SetWindowPos(window, 0, screens[0].Bounds.X, screens[0].Bounds.Y - wsMenuStrip, screens[0].Bounds.Width, screens[0].Bounds.Height + wsMenuStrip, 0x0040);
+                        DrawMenuBar(window);
+                        Taskbar.Hide();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Failed to properly place Cemu into borderless window mode.", "Cemu Borderless Window");
+                    }
+                }
+
+            }
+            else
+            {
+                try
+                {
+                    SetWindowLong(window, gwlStyle, wsCaption | wsBorder | wsSysMenu | wsMinimizeBox);
+                    SetWindowPos(window, 0, -6, 0, 1280, 770, 0x0040);
+                    DrawMenuBar(window);
+                    Taskbar.Show();
+                }
+                catch
+                {
+                    MessageBox.Show("Failed to properly place Cemu into windowed mode.", "Cemu Borderless Window");
+                }
+            }
+        }
+
+        private void chkCBW_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkIfProcessIsRunning("Cemu"))
+            {
+                setWindow();
+            }
+            else
+            {
+                if (chkCBW.CheckState == CheckState.Checked)
+                {
+                    MessageBox.Show("Please make sure Cemu is running before attempting to enable or disable borderless window mode.", "Cemu Borderless Window");
+                }
+                else
+                {
+                    MessageBox.Show("Please make sure Cemu is running before attempting to enable or disable borderless window mode.", "Cemu Borderless Window");
+                }
+            }
+            
+        }
+
+        private void chkShowMenuStrip_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkIfProcessIsRunning("Cemu"))
+            {
+                setWindow();
+            }
+            else
+            {
+                if (chkCBW.CheckState == CheckState.Checked)
+                {
+                    MessageBox.Show("Please make sure Cemu is running before attempting to enable or disable borderless window mode.", "Cemu Borderless Window");
+                }
+                else
+                {
+                    MessageBox.Show("Please make sure Cemu is running before attempting to enable or disable borderless window mode.", "Cemu Borderless Window");
+                }
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Taskbar.Show();
+            this.Icon = Properties.Resources.CBW;
+
+            if (checkIfProcessIsRunning("Cemu"))
+            {
+                window = Process.GetProcessesByName("Cemu")[0].MainWindowHandle;
+            }
+            else
+            {
+                MessageBox.Show("Please make sure Cemu is running before attempting to run CBW.", "Cemu Borderless Window");
+                Application.Exit();
+            }
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Taskbar.Show();
+        }
+    }
+}
