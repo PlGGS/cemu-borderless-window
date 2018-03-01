117cc3e3cfddb5ee491595f32eb3d8aa2044d508
PlGGS <bbor0422@gmail.com>
2018-02-28 20:04:19 -0600

    Added command line params

--- CBW/Program.cs
+++ CBW/Program.cs
@@ -12,7 +12,7 @@ namespace CBW
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
